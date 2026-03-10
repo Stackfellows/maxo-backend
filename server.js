9b929313c452202ba854ac3d07e363f4c9313c43
Stack Fellows <stackfellows684@gmail.com>
2026-03-11 00:21:01 +0500

    Add files via upload

--- server.js
+++ server.js
@@ -19,7 +19,13 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    origin: [
+        'http://localhost:5173',
+        'http://localhost:3000',
+        'https://maxo.stackfellows.com',
+        'https://maxo-backend.onrender.com',
+        'https://maxo.onrender.com'
+    ],
     credentials: true
 }));
 app.use(express.json());
